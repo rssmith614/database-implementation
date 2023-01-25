3067f85e2036f8b2e809b58aecdbc817e6bb92ab
Florin Rusu <frusu@ucmerced.edu>
2023-01-24 16:27:23 -0800

    Add SQLite + C code

---
+++ code/tests/test-sqlite.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+#include "Swapify.cc"
+#include "SQLiteODBC.h"
+
+using namespace std;
+
+
+int main (int argc, char* argv[]) {
+	if (argc != 2) {
+		cout << "Usage: main [sqlite_file]" << endl;
+		return -1;
+	}
+
+	SString dbFile(argv[1]);
+	SQLiteODBC sj(dbFile);
+	
+	sj.OpenConnection();
+
+	//sj.DropTables();
+	//sj.CreateTables();
+	//sj.PopulateTables();
+
+	SString maker("A"), product("Laptop");
+
+	sj.PCsByMaker(maker);
+	sj.ProductByMaker(product, maker);
+	sj.AllProductsByMaker(maker);
+
+	sj.CloseConnection();
+
+	return 0;
+}
